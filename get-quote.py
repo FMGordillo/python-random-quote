efe6946a04beca11a61b015766b7ee48bf6f87c6
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-04-18 01:32:53 -0300

    OPA

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,19 @@ def primary():
 
     f = open("quotes.txt")
     quotes = f.readlines()
+    quotes_quant = quotes.__len__()
+    random_quote = quotes[random.randint(0, quotes_quant)]
     f.close()
 
-    print(quotes[random.randint(0, quotes.__len__() - 1)])
+    print(random_quote, end="")
 
 
 if __name__ == "__main__":
-    primary()
+    while True:
+        user_input = input(
+            "Press Enter for a new quote, or C and Enter to close")
+        if user_input == "c":
+            print("Goodbye", end="")
+            break
+        if user_input == "":
+            primary()
